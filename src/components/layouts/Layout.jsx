22146be7d20c78c023c073fd5e48757da4358663
Justin <justinjones38@myyahoo.com>
2026-06-11 09:19:39 -0500

    added initial Quiz Setup Questions

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -1,13 +1,26 @@
 import styles from "./Layout.module.css"
 import { Outlet } from "react-router"
-
+import { useState } from "react";
 
 export default function Layout() {
+  const [quizQuestions, setQuizQuestions] = useState({
+    quizType: ""
+  });
+
+  const updateQuestions = (newName, newVal) => {
+    setQuizQuestions(prev => ({
+      ...prev,
+      [newName]: newVal
+    }))
+  }
+
+  console.log(quizQuestions);
+
   return (
     <div className={styles.mainContainer}>
       <h1 className={styles.title}>Quiz Time</h1>
       <div className={styles.bodyContainer}>
-        <Outlet />
+        <Outlet context={{quizQuestions, updateQuestions}} />
       </div>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,44 @@
 import styles from "./Home.module.css"
+import { useOutletContext } from "react-router"
 
 export default function Home() {
+  const {quizQuestions, updateQuestions} = useOutletContext()
   return (
   <div className={styles.container}>
     <h2 className={styles.pageTitle}>Welcome to Quiz Time!</h2>
     <p className={styles.description}>
-      Please
+      Please answer the questions below to get started with your questions
     </p>
+    <form className={styles.form}>
+      <fieldset>
+        <legend>What type of quiz do you want?</legend>
+        <div className={styles.questions}>
+          <label htmlFor="quiz-type">
+            <input 
+              type="radio" 
+              id="quiz-type" 
+              name="quizType" 
+              value="textBased" 
+              checked={quizQuestions.quizType === "textBased"} 
+              onChange={(e) => updateQuestions(e.target.name, e.target.value)}
+              />
+            Text-based
+          </label>
+          <label htmlFor="quiz-type">
+            <input 
+              type="radio" 
+              id="quiz-type" 
+              name="quizType" 
+              value="imageBased" 
+              checked={quizQuestions.quizType === "imageBased"} 
+              onChange={(e) => updateQuestions(e.target.name, e.target.value)}      
+              />
+            Image-based
+          </label>
+        </div>
+
+      </fieldset>
+    </form>
   </div>
   )
 }
