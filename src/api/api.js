068210f4aaba46678a5632590132425d850848f1
Justin <justinjones38@myyahoo.com>
2026-06-12 14:25:17 -0500

    added parameters for fetching data

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,13 @@
-export const fetchQuizQuestions = async(quizType, difficulty, categories) => {
+export const fetchQuizQuestions = async(categories=[], difficulty=[]) => {
+  const difficultyParams = difficulty.length > 0 ? 
+    `&difficulties=${[...difficulty].join(",")}` : 
+    "" 
+
+  const categoryParams = categories.length > 0 ? 
+    `&categories=${[...categories].join(",")}` : 
+    "" 
   try {
-    const res = await fetch(`https://the-trivia-api.com/v2/questions?contentFilter=family`);
+    const res = await fetch(`https://the-trivia-api.com/v2/questions?contentFilter=family${categoryParams}${difficultyParams}`);
     if(!res.ok) {
       throw new Error("cannot fetch data")
     }
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -4,27 +4,32 @@ import { fetchQuizQuestions } from "../api/api";
 import { useEffect, useState } from "react";
 
 export default function Quiz() {
-  const {setupAnswers} = useOutletContext()
-  const [data, setData] = useState({});
+  const { setupAnswers } = useOutletContext();
+  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(null);
   const [error, setError] = useState(false);
-  console.log(data);
-  useEffect(() => {
-    const fetchData = async() => {
-      setLoading(true);
-      try {
-        const fetchResults = await fetchQuizQuestions();
-        setData(fetchResults);
-
-      } catch(err) {
-        setError(true); 
-      } finally {
-        setLoading(false)
-      }
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const fetchResults = await fetchQuizQuestions(
+        setupAnswers.categories,
+        setupAnswers.difficulties,
+      );
+      setData(fetchResults);
+    } catch (err) {
+      setError(true);
+    } finally {
+      setLoading(false);
     }
+  };
+  useEffect(() => {
     fetchData();
-  }, [])
-  return ( 
-    <h1>test</h1>
+  }, []);
+  return (
+    <div className={styles.container}>
+      {error ? <h2>Error: Cannot fetch flashcards</h2> : null}
+      {loading ? <h2>Loading ...</h2> : null}
+      {!error && !loading && data ? <h2>test: fetched data</h2> : null}
+    </div>
   );
 }
