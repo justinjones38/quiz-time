f598f69f289ce33f995c2e1b70509a3bdba0a1ac
Justin <justinjones38@myyahoo.com>
2026-06-12 10:20:51 -0500

    added fetch logic for quiz

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,10 +1,30 @@
 import styles from "./Quiz.module.css";
 import { useOutletContext } from "react-router";
+import { fetchQuizQuestions } from "../api/api";
+import { useEffect, useState } from "react";
 
 export default function Quiz() {
   const {setupAnswers} = useOutletContext()
-  console.log(setupAnswers);
-  return (
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(false);
+  console.log(data);
+  useEffect(() => {
+    const fetchData = async() => {
+      setLoading(true);
+      try {
+        const fetchResults = await fetchQuizQuestions();
+        setData(fetchResults);
+
+      } catch(err) {
+        setError(true); 
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchData();
+  }, [])
+  return ( 
     <h1>test</h1>
   );
 }
