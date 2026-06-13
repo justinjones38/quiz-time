9f83f03ecca4c79d9d45aaed3fd0912b8e2d45b4
Justin <justinjones38@myyahoo.com>
2026-06-12 20:59:31 -0500

    added background colors for question difficulty

--- src/components/Flashcards.jsx
+++ src/components/Flashcards.jsx
@@ -1,25 +1,30 @@
 import { useState } from "react";
 import styles from "./Flashcards.module.css";
 
-export default function Flashcards({quizData}) {
+export default function Flashcards({ quizData }) {
   const [isQuestionShown, setIsQuestionShown] = useState(true);
   const [cardNumber, setCardNumber] = useState(0);
-  console.log(quizData[0])
-  console.log(isQuestionShown)
+  console.log(quizData[cardNumber]);
+
   return (
     <div className={styles.container}>
-      <div className={styles.flashcard} onClick={() => setIsQuestionShown(prev => !prev)}>
-        {isQuestionShown ? 
-        <div className={styles.frontCard}>
-          <p className={styles.question}>
-            {quizData[cardNumber].question.text}
-          </p>
-        </div> : 
-        <div className={styles.backCard}>
-          <p className={styles.answer}>
-            {quizData[cardNumber].correctAnswer}
-          </p>
-        </div> }
+      <div
+        className={`${styles["flashcard"]} ${styles[quizData[cardNumber].difficulty]} `}
+        onClick={() => setIsQuestionShown((prev) => !prev)}
+      >
+        {isQuestionShown ? (
+          <div className={styles.frontCard}>
+            <p className={styles.question}>
+              {quizData[cardNumber].question.text}
+            </p>
+          </div>
+        ) : (
+          <div className={styles.backCard}>
+            <p className={styles.answer}>
+              {quizData[cardNumber].correctAnswer}
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
